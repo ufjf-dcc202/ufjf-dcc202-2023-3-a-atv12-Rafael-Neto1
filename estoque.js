7f58cfb5ee3b8c506cdfe7d8896af57ba14900cc
Rafael Neto <rafael.neto@estudante.ufjf.br>
2023-12-06 21:04:16 -0300

    cria e edita estoque

---
+++ estoque.js
@@ -0,0 +1,15 @@
+//estoque.js
+let estoque = {
+joao: [{ 'tipo': "maca", qtd: 1 },
+            { 'tipo': "pera", qtd: 1 }
+ ],
+maria: [ { 'tipo': "maca", 'qtd': 2 }, 
+        { 'tipo': "banana", 'qtd': 4 }
+
+ ],
+}
+
+function getEstoque(){
+    return structuredClone(estoque);
+}
+//export {getEstoque}
